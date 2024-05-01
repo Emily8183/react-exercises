3110a787e709f453af37a07b632737bf11ce7bbd
Emily <emily.zy.lin@gmail.com>
2024-05-01 15:13:14 -0500

    finish 286 and add to 283

--- react/S283/S283/src/App.jsx
+++ react/S283/S283/src/App.jsx
@@ -2,10 +2,27 @@ import "./App.css";
 
 function App() {
   const name = "Emily";
-  const currentYear = new Date().getFullYear();
+
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  let greetings = "Good morning";
+
+  let customStyle = { color: "red" };
+
+  if (currentDate.getTime() > 12) {
+    greetings = "Good afternoon";
+    customStyle.color = "green";
+  } else if (currentDate.getTime() > 18) {
+    greetings = "Good evening";
+    customStyle.color = "blue";
+  }
 
   return (
     <div>
+      <h1 className="heading" style={customStyle}>
+        {greetings}
+      </h1>
       <p>Created by {name}</p>
       <p>Copyright {currentYear}</p>
     </div>
