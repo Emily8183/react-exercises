141f1400769ff5ddd837c2941f8c29235fa5feee
Emily <emily.zy.lin@gmail.com>
2024-05-01 14:43:36 -0500

    finish S284 and add to S281's file

--- S281/src/App.jsx
+++ S281/src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-// import "./App.css";
+import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -21,9 +21,23 @@ function App() {
 
       <ul>
         <li>Pizza</li>
+        <img
+          src="https://t3.ftcdn.net/jpg/01/33/61/72/240_F_133617244_dWdivRXwoLVzowl1kn3iFP9JGcuNd8n6.jpg"
+          className="image"
+          alt="pizza"
+        ></img>
         <li>Burger</li>
+        <img
+          src="https://www.foodandwine.com/thmb/jldKZBYIoXJWXodRE9ut87K8Mag=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/crispy-comte-cheesburgers-FT-RECIPE0921-6166c6552b7148e8a8561f7765ddf20b.jpg"
+          className="image"
+          alt="burger"
+        ></img>
         <li>Pasta</li>
-        <li>Fried Rice</li>
+        <img
+          src="https://www.allrecipes.com/thmb/JPQcpUKRsPXhUZm0H-XZUpjrp8w=/0x512/filters:no_upscale():max_bytes(150000):strip_icc()/67700_RichPastaforthePoorKitchen_ddmfs_4x3_2284-220302ec8328442096df370dede357d7.jpg"
+          className="image"
+          alt="pasta"
+        ></img>
       </ul>
     </>
   );
