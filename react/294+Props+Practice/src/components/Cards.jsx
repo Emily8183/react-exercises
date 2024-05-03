bc27a5e83493f4e7f245ad2a83a3b97b9250811e
Emily <emily.zy.lin@gmail.com>
2024-05-02 19:40:09 -0500

    finish session 296

--- react/294+Props+Practice/src/components/Cards.jsx
+++ react/294+Props+Practice/src/components/Cards.jsx
@@ -5,6 +5,7 @@ function Cards(props) {
   return (
     <div className="card">
       <div className="top">
+        <p>{props.id}</p>
         <h2 className="name">{props.name}</h2>
         <img className="circle-img" src={props.imgURL} alt="avatar_img" />
       </div>
