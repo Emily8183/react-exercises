02697d44a124dedb689fb765d0beddbca74f2d73
Emily <emily.zy.lin@gmail.com>
2024-05-04 11:06:54 -0500

    fix css and re-organize structure

---
+++ react/S305+useState/src/component/App.jsx
@@ -0,0 +1,15 @@
+import "../App.css";
+
+// Challenge1: Create a new component that shows the current time after clicking button, init value is TIME;
+// Challenge2: Create a new component that shows the current time without clicking button. Can use setInterval(code, delay);
+
+function App() {
+  return (
+    <div className="container">
+      <h1>TIME</h1>
+      <button>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
