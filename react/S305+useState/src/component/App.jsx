0cd460c349cec9f3009f94e57628a173064b9daa
Emily <emily.zy.lin@gmail.com>
2024-05-04 11:10:50 -0500

    finish Challenge 1

--- react/S305+useState/src/component/App.jsx
+++ react/S305+useState/src/component/App.jsx
@@ -1,13 +1,25 @@
 import "../App.css";
+import React, { useState } from "react";
 
 // Challenge1: Create a new component that shows the current time after clicking button, init value is TIME;
 // Challenge2: Create a new component that shows the current time without clicking button. Can use setInterval(code, delay);
 
 function App() {
+  const [time, setTime] = useState("TIME");
+
+  function updateTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const time = `${hours}:${minutes}:${seconds}`;
+    setTime(time);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
