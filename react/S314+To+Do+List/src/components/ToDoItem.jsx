c2c17bb2c6d89e9ae662fea37259b622ef3f24c5
Emily <emily.zy.lin@gmail.com>
2024-05-13 15:58:43 -0500

    finish S315 delete items from list

--- react/S314+To+Do+List/src/components/ToDoItem.jsx
+++ react/S314+To+Do+List/src/components/ToDoItem.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+//如何划去items:
+// 1)为map中的item创建useState组件
+// 2）用text-decoration:underline 来划线
+// 3）用ternary operator来判断划去还是不划去
+
+import React, { useState } from "react";
 
 function ToDoItem(props) {
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  function removeItems() {
+    setIsCompleted((prev) => !prev);
+  }
+
   return (
     <div>
-      <li>{props.text}</li>
+      <li
+        onClick={removeItems}
+        style={{ textDecoration: isCompleted ? "line-through" : "none" }}
+      >
+        {props.text}
+      </li>
     </div>
   );
 }
 // need the props to pass the todoItem which is a text
+// props is read-only, can't modify it
 
 export default ToDoItem;
