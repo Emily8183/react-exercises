064144061eb09fad6e56186dc10caaebd6a43d3b
Emily <emily.zy.lin@gmail.com>
2024-06-30 16:40:11 -0500

    add useEffect Sample

--- react/Samples/UseEffectSample.jsx
+++ react/Samples/UseEffectSample.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function UseStateSample() {
+function UseEffectSample() {
   const [count, setCount] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
   };
+
+  useEffect(() => {
+    console.log(`you clicked ${count} times`); //triger when count changes
+  }, [count]);
 
   return (
     <div>
